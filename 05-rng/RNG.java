369504c7f92335ff3e6b29b69d65cd2514ff9b7b
AP Java Period 5 <apjavaperiod5@g8-thinq.78b505.nycboe.org>
2022-10-04 11:37:42 -0400

    Random Number Generator (Ver. 1)

---
+++ 05-rng/RNG.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of class RNG here.
+ *
+ * Ying Shan Li
+ * 10/4/22
+ */
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class RNG {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    Random random = new Random();
+    int number = random.nextInt(100) + 1;
+    System.out.println("I am thinking of a number between 1 and 100");
+    System.out.println("(including both). Can you guess what it is?");
+    System.out.print("Type a number: ");
+    int guessNumber = in.nextInt();
+    System.out.println("Your guess is: " + guessNumber);
+    System.out.println("The number I was thinking of is: " + number);
+    System.out.println("You were off by: " + (number - guessNumber));
+  }
+}
