d2f965a882895a366cd250a0a53f9e6ce18a103c
AP Java Period 5 <apjavaperiod5@ac1mwk15bjc9h.78b505.nycboe.org>
2022-10-06 12:08:02 -0400

    Added different modes

--- 05-rng/RNG.java
+++ 05-rng/RNG.java
@@ -14,6 +14,19 @@ public class RNG {
     Scanner in = new Scanner(System.in);
     Random random = new Random();
     int number = random.nextInt(100) + 1;
+    
+    System.out.println("What mode would you like to play on? \nEasy \nMedium \nHard");
+    String mode = in.nextLine(); 
+    if(mode.toLowerCase().equals("easy")){
+        System.out.println(mode);
+    }
+    else if(mode.toLowerCase().equals("medium")){
+        System.out.println(mode);
+    }
+    else{
+        System.out.println(mode);
+    }
+    
     System.out.println("I am thinking of a number between 1 and 100");
     System.out.println("(including both). Can you guess what it is?");
     System.out.print("Type a number: ");
