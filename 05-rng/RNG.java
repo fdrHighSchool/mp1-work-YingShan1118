4021f68b5ed5d237b02045d38bbf075a77bfd9d5
AP Java Period 5 <apjavaperiod5@ac1mwk15bjc9h.78b505.nycboe.org>
2022-10-07 12:02:24 -0400

    Changed to Math.random()

--- 05-rng/RNG.java
+++ 05-rng/RNG.java
@@ -7,32 +7,31 @@
  */
 
 import java.util.Scanner;
-import java.util.Random;
 
 public class RNG {
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
-    Random random = new Random();
-    int number = random.nextInt(100) + 1;
     
+    int maximum;
     System.out.println("What mode would you like to play on? \nEasy \nMedium \nHard");
     String mode = in.nextLine(); 
     if(mode.toLowerCase().equals("easy")){
-        System.out.println(mode);
-    }
+        maximum = 10;
+    }//End if statement
     else if(mode.toLowerCase().equals("medium")){
-        System.out.println(mode);
-    }
+        maximum = 50;
+    }//End else if statement
     else{
-        System.out.println(mode);
-    }
+        maximum = 100;
+    }//End else statement 
     
-    System.out.println("I am thinking of a number between 1 and 100");
+    int randomNumber = (int)((Math.random() + 1) * maximum);
+    System.out.println("I am thinking of a number between 1 and " + maximum);
     System.out.println("(including both). Can you guess what it is?");
     System.out.print("Type a number: ");
     int guessNumber = in.nextInt();
     System.out.println("Your guess is: " + guessNumber);
-    System.out.println("The number I was thinking of is: " + number);
-    System.out.println("You were off by: " + (number - guessNumber));
+    System.out.println("The number I was thinking of is: " + randomNumber);
+    System.out.println("You were off by: " + (randomNumber - guessNumber));
   }
 }
