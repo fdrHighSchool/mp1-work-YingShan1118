045401578ce26a05c30723b423e1434d2fb742a8
AP Java Period 5 <apjavaperiod5@ac1mwk15bjc9h.78b505.nycboe.org>
2022-10-11 12:03:43 -0400

    User choose the range of the rng

--- 05-rng/RNG.java
+++ 05-rng/RNG.java
@@ -1,6 +1,6 @@
 
 /**
- * Write a description of class RNG here.
+ * Write a description of class rng here.
  *
  * Ying Shan Li
  * 10/4/22
@@ -8,25 +8,18 @@
 
 import java.util.Scanner;
 
-public class RNG {
+public class rng {
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
     
-    int maximum;
-    System.out.println("What mode would you like to play on? \nEasy \nMedium \nHard");
-    String mode = in.nextLine(); 
-    if(mode.toLowerCase().equals("easy")){
-        maximum = 10;
-    }//End if statement
-    else if(mode.toLowerCase().equals("medium")){
-        maximum = 50;
-    }//End else if statement
-    else{
-        maximum = 100;
-    }//End else statement 
+    System.out.println("What would you like the minimum number to be?");
+    int minimum = in.nextInt(); 
     
-    int randomNumber = (int)((Math.random() + 1) * maximum);
-    System.out.println("I am thinking of a number between 1 and " + maximum);
+    System.out.println("What would you like the minimum number to be?");
+    int maximum = in.nextInt(); 
+    
+    int randomNumber = (int)((Math.random() * (maximum - minimum + 1) + minimum));
+    System.out.println("I am thinking of a number between " + minimum + " and " + maximum);
     System.out.println("(including both). Can you guess what it is?");
     System.out.print("Type a number: ");
     int guessNumber = in.nextInt();
